291540ab603254ca531c0e8e1d9f950232f40270
Paolacocc <coccettipaola@gmail.com>
2023-06-01 17:30:42 +0200

    Add personalized error on store

--- app/Http/Requests/StoreComicRequest.php
+++ app/Http/Requests/StoreComicRequest.php
@@ -25,7 +25,7 @@ class StoreComicRequest extends FormRequest
     {
         return [
             'title' => 'required|min:3|max:50',
-            'description' => 'required|min:5|max:255',
+            'description' => 'required',
             'thumb' => 'required',
             'price' => 'required|min:2|max:6',
             'series' => 'required|min:5|max:255',
@@ -40,9 +40,23 @@ class StoreComicRequest extends FormRequest
      */
     public function messages() {
         return [
-            'title.required' => 'the title is required',
-            'title.min' => 'the title have to be longer then :min characters',
-            'title.max' => 'the title cannot be longer then :max characters' 
+            'title.required' => "Il titolo e' obbligatorio",
+            'title.min' => "Il titolo non puo' essere piu' corto di :min caratteri",
+            'title.max' => "Il titolo puo' essere lungo :max caratteri massimo",
+            'description.required' => "La descrizione e' obbligatoria", 
+            'thumb.required' => "La foto e' obbligatoria",
+            'price.required' => "Il prezzo e' obbligatorio",
+            'price.min' => "Il prezzo non puo' essere meno di :min caratteri",
+            'price.max' => "Il prezzo non puo' essere piu di :max caratteri",
+            'series.required' => "La serie e' obbligatoria",
+            'series.min' => "La serie non puo' avere meno di :min caratteri",
+            'series.max' => "La serie puo' avere un massimo di :max caratteri",
+            'sale_date.required' => "La data e' obbligatoria",
+            'sale_date.min' => "La data non puo' avere meno di :min caratteri",
+            'sale_date.max' => "La data non puo' avere piu' di :max caratteri",
+            'type.required' => "La tipologia e' obbligatoria",
+            'type.min' => "La tipologia non puo' avere meno di :min caratteri",
+            'type.max' => "La tipologia non puo' avere piu' di :max caratteri"
         ];
     }
 }
--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,53 +1,89 @@
 @extends('layouts.app')
 @section('content')
-<div class="container mt-4">
+    <div class="container mt-4">
 
-    <h3>Make your changes</h3>
-        <form action="{{ route('comic.store')}}" method="POST">
-        @csrf
-        <div class="mb-4">
-            <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title')}}">
-            @error('title')
+        <h3>Make your changes</h3>
+        <form action="{{ route('comic.store') }}" method="POST">
+            @csrf
+            <div class="mb-4">
+                <label for="title" class="form-label">Title</label>
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
+                    value="{{ old('title') }}">
+                @error('title')
                     <div class="invalid-feedback">
                         {{ $message }}
                     </div>
                 @enderror
-        </div>
+            </div>
 
-        <div class="mb-3">
-            <label for="description" class="form-label">Description</label>
-            <textarea class="form-control" name="description" id="description" rows="3"></textarea>
-        </div>
+            <div class="mb-3">
+                <label for="description" class="form-label">Description</label>
+                <textarea class="form-control @error('title') is-invalid @enderror" name="description" id="description" rows="3">{{ old('description') }}</textarea>
+                @error('description')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+            </div>
 
-        <div class="mb-3">
-            <label for="thumb" class="form-label">Image</label>
-            <input type="text" class="form-control" id="thumb" name="thumb">
-        </div>
+            <div class="mb-3">
+                <label for="thumb" class="form-label">Image</label>
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="thumb"
+                    name="thumb" value="{{ old('thumb') }}">
+                    @error('thumb')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
 
 
-        <div class="mb-3">
-            <label for="price" class="form-label">price</label>
-            <input type="text" class="form-control" id="price" name="price">
-        </div>
-        
-        <div class="mb-3">
-            <label for="series" class="form-label">Series</label>
-            <input type="text" class="form-control" name="series" id="series">
-        </div>
+            <div class="mb-3">
+                <label for="price" class="form-label">price</label>
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="price"
+                    name="price" value="{{ old('price') }}">
+                    @error('price')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
 
-        <div class="mb-3">
-            <label for="sale-date" class="form-label">Sale date</label>
-            <input type="text" class="form-control" id="sale-date" name="sale_date">
-        </div>
+            <div class="mb-3">
+                <label for="series" class="form-label">Series</label>
+                <input type="text" class="form-control @error('title') is-invalid @enderror" name="series"
+                    id="series" value="{{ old('series') }}">
+                    @error('series')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
 
-        <div class="mb-3">
-            <label for="type" class="form-label">Type</label>
-            <input type="text" class="form-control" id="type" name="type">
-        </div>
-    
-        <button class="btn btn-warning" type="submit">Send</button>
-    
+            <div class="mb-3">
+                <label for="sale-date" class="form-label">Sale date</label>
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="sale-date"
+                    name="sale_date" value="{{ old('sale_date') }}">
+                    @error('sale_date')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+
+            <div class="mb-3">
+                <label for="type" class="form-label">Type</label>
+                <input type="text" class="form-control @error('type') is-invalid @enderror" id="type"
+                    name="type" value="{{ old('type') }}">
+                    @error('type')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+
+            <button class="btn btn-warning" type="submit">Send</button>
+
         </form>
-</div>
+    </div>
 @endsection
